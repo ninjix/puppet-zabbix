2921731b79de18397c758a354a0d51b4ac6aefc0
Rik Bruggink <mail@rikbruggink.nl>
2013-04-11 11:52:57 +0200

    changed package to zabbix20
    changed package to zabbix20 to reflect epel packaging

--- manifests/agent.pp
+++ manifests/agent.pp
@@ -3,7 +3,7 @@ class zabbix::agent (
   $enable          = true,
   $general_options = $zabbix::params::general_options,
   $agent_options   = $zabbix::params::agent_options) inherits zabbix::params {
-  package { "zabbix-agent": ensure => installed }
+  package { "zabbix-agent20": ensure => installed }
 
   $config_file = "${general_options[config_dir]}/${agent_options[config]}"
   $include_dir = "${general_options[config_dir]}/${agent_options[include_dir]}/"
@@ -15,43 +15,43 @@ class zabbix::agent (
       owner   => 'root',
       group   => 'root',
       mode    => '0755',
-      require => Package["zabbix-agent"];
+      require => Package["zabbix-agent20"];
 
     $include_dir:
       ensure  => directory,
       owner   => 'zabbix',
       group   => 'zabbix',
       mode    => '0700',
-      require => Package["zabbix-agent"];
+      require => Package["zabbix-agent20"];
 
     $script_dir:
       ensure  => directory,
       owner   => 'zabbix',
       group   => 'zabbix',
       mode    => '0700',
-      require => Package["zabbix-agent"];
+      require => Package["zabbix-agent20"];
 
     $config_file:
       owner   => 'root',
       group   => 'root',
       mode    => '0644',
       content => template("zabbix/zabbix_agentd_conf.erb"),
-      require => Package["zabbix-agent"];
+      require => Package["zabbix-agent20"];
 
     $agent_options[log_dir]:
       ensure  => directory,
       owner   => 'zabbix',
       group   => 'zabbix',
       mode    => '0755',
-      require => Package["zabbix-agent"];
+      require => Package["zabbix-agent20"];
   }
 
   service { "zabbix-agent":
     enable    => true,
     ensure    => running,
     hasstatus => true,
-    require   => Package["zabbix-agent"],
-    subscribe => [File[$config_file], Package["zabbix-agent"]];
+    require   => Package["zabbix-agent20"],
+    subscribe => [File[$config_file], Package["zabbix-agent20"]];
   }
 
 }

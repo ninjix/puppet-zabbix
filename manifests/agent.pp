0f706284a96d05ef6c4775dc95652298a2cc5b91
Rik Bruggink <mail@rikbruggink.nl>
2013-04-11 14:14:36 +0200

    added case osfamily to puppet module

--- manifests/agent.pp
+++ manifests/agent.pp
@@ -3,7 +3,10 @@ class zabbix::agent (
   $enable          = true,
   $general_options = $zabbix::params::general_options,
   $agent_options   = $zabbix::params::agent_options) inherits zabbix::params {
-  package { "zabbix20-agent": ensure => installed }
+ 
+ case $osfamily {
+	RedHat  : {
+    package { "zabbix20-agent": ensure => installed }
 
   $config_file = "${agent_options[config]}"
   $include_dir = "${general_options[config_dir]}/${agent_options[include_dir]}/"
@@ -53,5 +56,60 @@ class zabbix::agent (
     require   => Package["zabbix20-agent"],
     subscribe => [File[$config_file], Package["zabbix20-agent"]];
   }
+  }
+  Debian  : {
+  package { "zabbix-agent": ensure => installed }
+
+  $config_file = "${general_options[config_dir]}/${agent_options[config]}"
+  $include_dir = "${general_options[config_dir]}/${agent_options[include_dir]}/"
+  $script_dir = "${general_options[config_dir]}/${agent_options[script_dir]}/"
+
+  file {
+    $general_options[config_dir]:
+      ensure => directory,
+      owner => 'root',
+      group => 'root',
+      mode => '0755',
+      require => Package["zabbix-agent"];
+
+    $include_dir:
+      ensure => directory,
+      owner => 'zabbix',
+      group => 'zabbix',
+      mode => '0700',
+      require => Package["zabbix-agent"];
+
+    $script_dir:
+      ensure => directory,
+      owner => 'zabbix',
+      group => 'zabbix',
+      mode => '0700',
+      require => Package["zabbix-agent"];
+
+    $config_file:
+      owner => 'root',
+      group => 'root',
+      mode => '0644',
+      content => template("zabbix/zabbix_agentd_conf.erb"),
+      require => Package["zabbix-agent"];
+
+    $agent_options[log_dir]:
+      ensure => directory,
+      owner => 'zabbix',
+      group => 'zabbix',
+      mode => '0755',
+      require => Package["zabbix-agent"];
+  }
+
+  service { "zabbix-agent":
+    enable => true,
+    ensure => running,
+    hasstatus => true,
+    require => Package["zabbix-agent"],
+    subscribe => [File[$config_file], Package["zabbix-agent"]];
+  }
 
 }
+}
+}
+
